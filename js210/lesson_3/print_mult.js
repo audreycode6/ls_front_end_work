3e4091308badcf89540a6204f5130286a020f294
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-05-15 09:59:20 -0700

    refactor solutuion to loop through numbers descending from 100 to number to find multiples

--- js210/lesson_3/print_mult.js
+++ js210/lesson_3/print_mult.js
@@ -8,23 +8,11 @@ You may assume that number is an integer between 0 and 100.
 */
 
 function logMultiples(number) {
-    if (number === 0) return;
-
-    let multiple = 0;
-    let multiples = []
-
-    for (let i = 0; multiple < 100; i++) {
-        multiple = number * i;
-        if (multiple > 100) break;
-
-        if (multiple % 2 !== 0) {
-            multiples.push(multiple);
-        }
+   for (let multiplier = 100; multiplier >= number; multiplier--) {
+    if (multiplier % 2 === 1 && multiplier % number === 0) {
+        console.log(multiplier);
     }
-
-    while (multiples.length > 0) {
-        console.log(multiples.pop());
-    }
+   }
 }
 
 logMultiples(17);
