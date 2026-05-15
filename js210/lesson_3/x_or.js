faf8f3619ea0a35201575446a1f4d3c41d4b04e1
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-05-15 13:56:59 -0700

    Practice Problems: Logic and Flow Control

---
+++ js210/lesson_3/x_or.js
@@ -0,0 +1,30 @@
+/* 
+Write a function named isXor that takes two arguments, 
+and returns true if exactly one argument is truthy, 
+false otherwise. Your function should work with the boolean 
+values of true and false, but also any JavaScript values 
+based on their "truthiness". 
+*/
+
+let isXor = (arg1, arg2) => {
+    arg1 = !!arg1;
+    arg2 = !!arg2;
+
+    if (arg1 && arg2) {
+        return false;
+    }
+
+    return arg1 || arg2;
+}
+
+
+console.log(isXor(false, true));     // true
+console.log(isXor(true, false));     // true
+console.log(isXor(false, false));    // false
+console.log(isXor(true, true));    // false
+
+
+console.log(isXor(false, 3));        // true
+console.log(isXor('a', undefined));  // true
+console.log(isXor(null, ''));        // false
+console.log(isXor('2', 23));         // false
