79b4daa6907f4d11d7615dd2c184e0468d186f04
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-05-15 14:27:09 -0700

    Practice Problems: Logic and Flow Control

---
+++ js210/lesson_3/guess_password.js
@@ -0,0 +1,26 @@
+/* 
+Write a password guessing program that tracks how 
+many times the user enters the wrong password. If the 
+user enters the password wrong three times, log 'You 
+have been denied access.' and terminate the program. 
+If the password is correct, log 'You have successfully 
+logged in.' and end the program.
+*/
+let rlSync = require('readline-sync');
+
+function secure_login_message() {
+    let validPw = 'password';
+    let pwAttemptCount = 0;
+    let pwInput = '';
+
+    while (pwAttemptCount < 3) {
+        let pwInput = rlSync.question('What is the password: ');
+        
+        if (pwInput === validPw) {
+            return 'You have successfully logged in.'; 
+        } else pwAttemptCount++;
+    }
+    return 'You have been denied access.'; 
+}
+
+console.log(secure_login_message());
