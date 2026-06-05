a0c9663d01a37c3c643f6a6a390296b27212e49d
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-05 12:59:39 -0700

    medium 2 problem: doubler

--- js210/javascript_basics/doubler.js
+++ js210/javascript_basics/doubler.js
@@ -16,3 +16,9 @@ const doubler = makeDoubler('Victor');
 console.log(doubler(5));                             // returns 10
 // logs:
 // This function was called by Victor.
+
+
+//welcome
+//to
+//the
+//matrix!
