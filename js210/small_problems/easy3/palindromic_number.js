25dbd24c7c9b2163017d484d75b4c815ccb69350
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-06 11:59:41 -0700

    function that returns true if its integer argument is palindromic, or false otherwise.

---
+++ js210/small_problems/easy3/palindromic_number.js
@@ -0,0 +1,20 @@
+/*
+Write a function that returns true if its integer argument is palindromic, or 
+false otherwise. A palindromic number reads the same forwards and backwards.
+ */
+
+const { isPalindrome } = require('./palindromic1');
+
+function isPalindromicNumber(number) {
+    //convert to string and then reverse and compare
+    return isPalindrome(String(number))
+}
+
+if (require.main === module) {
+    console.log(isPalindromicNumber(34543));        // true
+    console.log(isPalindromicNumber(123210));       // false
+    console.log(isPalindromicNumber(22));           // true
+    console.log(isPalindromicNumber(5));            // true
+}
+
+module.exports = { isPalindromicNumber };
