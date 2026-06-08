aa3498e6f66a4b35ef4cd8eea7cab7dcdc0dd6e3
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-08 16:55:53 -0700

    function that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the argument is a negative number, return it as-is.

---
+++ js210/small_problems/easy5/always_negative.js
@@ -0,0 +1,15 @@
+/*
+Write a function that takes a number as an argument. If the argument is a 
+positive number, return the negative of that number. If the argument is 
+a negative number, return it as-is.
+*/
+
+function negative(number) {
+    return  number < 0 ? number : -number
+}
+
+if (require.main === module) {
+    console.log(negative(5)) // -5
+    console.log(negative(-3)) // -3
+    console.log(negative(0)) // -0
+};
