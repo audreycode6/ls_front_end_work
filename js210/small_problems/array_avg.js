ef8770268e41066a8fab4160d6b7a67e6e3317f1
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-08 14:56:46 -0700

    function that takes one argument, an array containing integers, and returns the average of all the integers in the array, rounded down to the integer component of the average.

---
+++ js210/small_problems/array_avg.js
@@ -0,0 +1,22 @@
+/*
+Write a function that takes one argument, an array containing 
+integers, and returns the average of all the integers in the 
+array, rounded down to the integer component of the average. 
+The array will never be empty, and the numbers will always 
+be positive integers.
+*/
+
+
+function average(array) {
+    const sum = array.reduce((acc, curr) => {
+        return acc + curr;
+    });
+
+    return Math.floor(sum / array.length);
+}
+
+
+if (require.main === module) {
+    console.log(average([1, 5, 87, 45, 8, 8])) // 25
+    console.log(average([9, 47, 23, 95, 16, 52])) // 40
+};
