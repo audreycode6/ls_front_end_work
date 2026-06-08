64586eedf4510c3196dc09c6291df523e66656f1
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-08 15:19:45 -0700

    clean up: remove extra empty lines

--- js210/small_problems/easy5/double_char1.js
+++ js210/small_problems/easy5/double_char1.js
@@ -3,12 +3,10 @@ Write a function that takes a string, doubles every
 character in the string, and returns the result as a new string.
 */
 
-
 function repeater(string) {
     const newStringArray = [...string].map((char) => char += char )
     return newStringArray.join("");
 }
-
 
 if (require.main === module ){
     console.log(repeater('Hello')); // "HHeelllloo"
