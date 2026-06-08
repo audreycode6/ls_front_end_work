1a32d796b1967248337f282de0a15e598c0895ac
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-08 15:18:45 -0700

    function that takes a string, doubles every character in the string, and returns the result as a new string.

---
+++ js210/small_problems/easy5/double_char1.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes a string, doubles every 
+character in the string, and returns the result as a new string.
+*/
+
+
+function repeater(string) {
+    const newStringArray = [...string].map((char) => char += char )
+    return newStringArray.join("");
+}
+
+
+if (require.main === module ){
+    console.log(repeater('Hello')); // "HHeelllloo"
+    console.log(repeater('Good job!')); // "GGoooodd  jjoobb!!"
+    console.log(repeater('')); // ""
+};
