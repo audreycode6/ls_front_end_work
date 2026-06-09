dd74dfa6a6c3a54bc4fe6bad41049765698709d2
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-08 17:04:57 -0700

    function that takes an integer argument and returns an array containing all integers between 1 and the argument (inclusive), in ascending order

---
+++ js210/small_problems/easy5/counting_up.js
@@ -0,0 +1,22 @@
+/*
+Write a function that takes an integer argument and returns an 
+array containing all integers between 1 and the argument (inclusive), 
+in ascending order.
+
+You may assume that the argument will always be a positive integer. */
+
+
+function sequence(num) {
+    let numSequence = [];
+
+    for (let i = 1; i <= num; i++) {
+        numSequence.push(i);
+    }
+    return numSequence;
+}
+
+if (require.main === module) {
+    console.log(sequence(5)); // [1, 2, 3, 4, 5]
+    console.log(sequence(3)); // [1, 2, 3]
+    console.log(sequence(1)); // [1]
+};
