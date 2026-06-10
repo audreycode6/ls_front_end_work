389952fddf79c3b98ae9d2c6b43f7babe187626d
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-09 20:50:49 -0700

    clean up : linting indentation

--- js210/small_problems/easy1/even_nums.js
+++ js210/small_problems/easy1/even_nums.js
@@ -3,7 +3,6 @@ Log all even numbers from 1 to 99, inclusive,
 to the console, with each number on a separate line.
  */
 
-
 for (let num = 2; num < 100; num += 2) {
-    console.log(num);
+  console.log(num);
 }
