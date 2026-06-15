503308f6df2575a371efe157c59f7ec27c6b2b4f
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-15 11:10:29 -0700

    function that returns a list of all substrings of a string. Order the returned list by where in the string the substring begins.

---
+++ js210/list_processing/all_substrs.js
@@ -0,0 +1,49 @@
+/*
+Write a function that returns a list of all substrings
+of a string. Order the returned list by where in the string
+the substring begins. This means that all substrings that
+start at index position 0 should come first, then all
+substrings that start at index position 1, and so on. Since
+multiple substrings will occur at each position, return the
+substrings at a given index from shortest to longest.
+
+You may (and should) use the leadingSubstrings function
+you wrote in the previous exercise:
+*/
+
+const leadingSubstrings = require('./leading_substrings.js');
+
+function substrings(string) {
+  let allSubstrs = [];
+  [...string].forEach((_, idx, arr) => {
+    let currSubStr = arr.slice(idx);
+    let subStrs = leadingSubstrings(currSubStr);
+
+    subStrs.forEach((subStrGroup) => {
+      allSubstrs.push(subStrGroup.join(''));
+    });
+  });
+
+  return allSubstrs;
+}
+
+console.log(substrings('abcde'));
+
+// returns
+// [
+//   'a',
+//   'ab',
+//   'abc',
+//   'abcd',
+//   'abcde',
+//   'b',
+//   'bc',
+//   'bcd',
+//   'bcde',
+//   'c',
+//   'cd',
+//   'cde',
+//   'd',
+//   'de',
+//   'e',
+// ];
