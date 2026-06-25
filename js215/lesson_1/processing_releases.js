c825659b2e821bea9c69894e0845bba462df90dc
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-25 14:42:33 -0700

    refactor: handle id having value be 0 --> add condition to filtering

--- js215/lesson_1/processing_releases.js
+++ js215/lesson_1/processing_releases.js
@@ -27,7 +27,7 @@ let newReleases = [
     bookmark: [],
   },
   {
-    id: 675465,
+    id: 0,
     title: 'Fracture',
     boxart: 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
     uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
@@ -45,9 +45,18 @@ and non-empty string respectively. Here are the rules:
 Keep only releases that have both id and title property present.
 Keep only the id and title data for each release. */
 
+/*Part 2:
+current solution assumes that the value of id will be an
+integer value greater than 0. If it was possible to have
+a value of 0 for id, what would the implications be to the
+current solution? What changes, if any, would need to be
+made in order to handle the 0 value? */
+
+// check if id != undefined
+
 function processReleaseData(data) {
   let datasTitleAndId = data
-    .filter((elem) => elem.id && elem.title) // get elements that have a title and id key
+    .filter((elem) => elem.id !== undefined && elem.title) // get elements that have a title and id key
     .map((elem) => {
       // create array of just title and id KVP from filteredData
       return { title: elem.title, id: elem.id };
