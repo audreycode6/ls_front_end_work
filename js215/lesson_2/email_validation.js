5a7901374c820e43386d6bf8a371884939524216
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-06-29 17:18:57 -0700

    add export and condition to only output func invocation if in main module

--- js215/lesson_2/email_validation.js
+++ js215/lesson_2/email_validation.js
@@ -32,18 +32,22 @@ function isValidEmail(email) {
   return isAlphaNumericString(local) && validDomain(domain);
 }
 
-console.log(isValidEmail('Foo@baz.com.ph')); // returns true
-console.log(isValidEmail('Foo@mx.baz.com.ph')); // returns true
-console.log(isValidEmail('foo@baz.com')); // returns true
-console.log(isValidEmail('foo@baz.ph')); // returns true
-console.log(isValidEmail('foo@baz@bar.ph')); // returns false
-console.log(isValidEmail('HELLO123@baz')); // returns false
-console.log(isValidEmail('foo.bar@baz.to')); // returns false
-console.log(isValidEmail('foo@baz.')); // returns false
-console.log(isValidEmail('foo_bat@baz')); // returns false
-console.log(isValidEmail('foo@bar.a12')); // returns false
-console.log(isValidEmail('foo_bar@baz.com')); // returns false
-console.log(isValidEmail('foo@bar.....com')); // returns false
+module.exports = { isValidEmail };
+
+if (require.main === module) {
+  console.log(isValidEmail('Foo@baz.com.ph')); // returns true
+  console.log(isValidEmail('Foo@mx.baz.com.ph')); // returns true
+  console.log(isValidEmail('foo@baz.com')); // returns true
+  console.log(isValidEmail('foo@baz.ph')); // returns true
+  console.log(isValidEmail('foo@baz@bar.ph')); // returns false
+  console.log(isValidEmail('HELLO123@baz')); // returns false
+  console.log(isValidEmail('foo.bar@baz.to')); // returns false
+  console.log(isValidEmail('foo@baz.')); // returns false
+  console.log(isValidEmail('foo_bat@baz')); // returns false
+  console.log(isValidEmail('foo@bar.a12')); // returns false
+  console.log(isValidEmail('foo_bar@baz.com')); // returns false
+  console.log(isValidEmail('foo@bar.....com')); // returns false
+}
 
 /*
 -------------
