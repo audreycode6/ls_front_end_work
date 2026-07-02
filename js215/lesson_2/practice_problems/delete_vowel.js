2e068176d73a18a565e9e57a1a8e9467db99598d
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-07-02 10:39:16 -0700

    revisiting practice problem from js210:  function that takes an array of strings and returns  an array of the same string values, but with all vowels   (a, e, i, o, u) removed.

---
+++ js215/lesson_2/practice_problems/delete_vowel.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes an array of strings and returns
+ an array of the same string values, but with all vowels
+  (a, e, i, o, u) removed.
+ */
+
+function removeVowels(arrayOfStrings) {
+  const REGEX_VOWELS_CASE_INSENSITIVE = /[aeiou]/gi;
+
+  return arrayOfStrings.map((string) =>
+    string.replace(REGEX_VOWELS_CASE_INSENSITIVE, ''),
+  );
+}
+
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz'])); // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white'])); // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ'])); // ["BC", "", "XYZ"]
